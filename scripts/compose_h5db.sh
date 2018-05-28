1a4d71d3c0ab6a48634172da7e8adb878c4e52d6
batikim09 <j.kim@utwente.nl>
2018-05-28 08:51:01 +0200

    privacy data is cleaned.

--- scripts/compose_h5db.sh
+++ scripts/compose_h5db.sh
@@ -9,5 +9,5 @@ python ./src/h5db_builder.py -input ./meta/sanity.enterface.txt.mspec.out -m_ste
 #The following script composes a feature DB by using aggregated corpora.
 #specify corpus ID (c_idx) and choose multiple corpora indices  (c_ids)
 #To keep speaker independence, specify the index of speaker column (s_idx).
-python ./src/h5db_builder_cc_sid.py -input ./meta/sanity.aibo_enterface.txt.mspec.out -m_steps 100 -c_ids 0,1 -c_idx 3 -s_idx 2 -c_len 10 --three_d -mt 1:2:4:5:6:7 -out ./h5db/AE.RAW.3cls.av
+python ./src/h5db_builder_cc_sid.py -input ./meta/sanity.aibo_enterface.txt.mspec.out -m_steps 100 -c_ids 0,1 -c_idx 3 -s_idx 2 -c_len 10 --two_d -mt 1:2:4:5:6:7 -out ./h5db/AE.RAW.2d.3cls.av
 
